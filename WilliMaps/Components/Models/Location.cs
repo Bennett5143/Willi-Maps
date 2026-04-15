f03185c2a4bc760b60454a3ac9f0ae9bd09592b7
erik-gzn <186281172+erik-gzn@users.noreply.github.com>
2026-04-15 09:54:35 +0200

    Klasse Location hinzugefügt

---
+++ WilliMaps/Components/Models/Location.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace WilliMaps.Components.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Point Coordinates { get; set; }
+
+        public string Category { get; set; }
+
+        public List<Property> Properties { get; set; }
+
+        public List<OpeningTime> OpeningTimes { get; set; }
+
+
+    }
+}
