6c75c3859a3a20f6ab0479c6b332b9c4bf7d4e5b
erik-gzn <186281172+erik-gzn@users.noreply.github.com>
2026-04-24 11:30:15 +0200

    DivideByZero ergänzt

---
+++ WilliMaps/Components/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace WilliMaps.Components
+{
+    public class Calculator
+    {
+
+        public int add(int a,  int b) { return a + b; }
+
+        public int subtract(int a, int b) { return a - b; }
+
+        public int multiply(int a, int b) {return a * b; }
+
+        public double divide(int a, double b) 
+        {
+            if (b ==  0) throw new DivideByZeroException();
+            return a / b; 
+        }
+    }
+}
