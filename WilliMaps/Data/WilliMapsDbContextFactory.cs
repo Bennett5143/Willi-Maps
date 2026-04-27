07f9debdde757064cf1d2aa4174c8f73bdfc7e6b
Bennett-TZ <bennettsteenfatt@gmail.com>
2026-04-27 14:08:12 +0200

    Added some Security by removing hardcoded passwords

--- WilliMaps/Data/WilliMapsDbContextFactory.cs
+++ WilliMaps/Data/WilliMapsDbContextFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
 
 namespace WilliMaps.Data
@@ -8,14 +9,20 @@ namespace WilliMaps.Data
     {
         public WilliMapsDbContext CreateDbContext(string[] args)
         {
-            var dataSourceBuilder = new NpgsqlDataSourceBuilder(
-                "Host=localhost;Port=5433;Database=willimaps;Username=willimaps;Password=root"
-            );
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: true) 
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
             dataSourceBuilder.UseNetTopologySuite();
             var dataSource = dataSourceBuilder.Build();
 
             var optionsBuilder = new DbContextOptionsBuilder<WilliMapsDbContext>();
-            optionsBuilder.UseNpgsql(dataSource, o => o.UseNetTopologySuite()); // ← hier auch
+            optionsBuilder.UseNpgsql(dataSource, o => o.UseNetTopologySuite());
 
             return new WilliMapsDbContext(optionsBuilder.Options);
         }
